1d6b627bf1e694b1b3d3c0de108556186a852144
luciarosalesarcedv <lucia.rosalesarce@davinci.edu.ar>
2024-07-11 01:22:17 -0300

    update

--- Assets/Scripts/Menu/Botones_Menu.cs
+++ Assets/Scripts/Menu/Botones_Menu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Botones_Menu : MonoBehaviour
@@ -13,6 +14,5 @@ public class Botones_Menu : MonoBehaviour
    public void Salir()
     {
         Application.Quit();
-        Debug.Log("Si funciona salir");
     }
 }
